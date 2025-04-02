8a9f633a85e95753c723cb4b7f8f81890962694f
sbomintegrity <sbom.integrity@gmail.com>
2025-04-02 10:00:05 +0200

    Add files via upload

---
+++ Generation/Csharp/HelloDotNet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace HelloDotNet
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person { Name = "Alice", Age = 30 };
+            string json = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine("Serialized JSON:");
+            Console.WriteLine(json);
+        }
+    }
+}
